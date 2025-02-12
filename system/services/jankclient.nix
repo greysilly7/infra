27882ea35b29c8895d27cf1fa7c733dbcef16e2a
greysilly7 <greysilly7@gmail.com>
2025-02-12 10:43:04 -0500

    feat: fix jankclient service to correct directory path for bun commands
    Signed-off-by: greysilly7 <greysilly7@gmail.com>

--- system/services/jankclient.nix
+++ system/services/jankclient.nix
@@ -21,8 +21,8 @@ in {
       # ${pkgs.coreutils}/bin/mkdir -p ${writableDir}
       # ${pkgs.coreutils}/bin/chown -R jankclient:jankclient ${writableDir}
       ${pkgs.coreutils}/bin/cp -r ${inputs.jankclient}/* ${writableDir}/gitfiles
-      ${lib.getExe pkgs.bun} install --cwd ${writableDir}
-      ${lib.getExe pkgs.bun} gulp --cwd ${writableDir} --swc
+      ${lib.getExe pkgs.bun} install --cwd ${writableDir}/gitfules
+      ${lib.getExe pkgs.bun} gulp --cwd ${writableDir}/gitfiles --swc
     '';
 
     # script = "${inputs.jankwrapper.packages.${pkgs.system}.default}/bin/jankwrapper";
