4246550e2fab74e4b32d68a16d3a1522c7f735b5
greysilly7 <greysilly7@gmail.com>
2025-02-12 10:33:06 -0500

    feat: update jankclient service to change directory ownership to jankclient user
    Signed-off-by: greysilly7 <greysilly7@gmail.com>

--- system/services/jankclient.nix
+++ system/services/jankclient.nix
@@ -19,7 +19,7 @@ in {
 
     preStart = ''
       ${pkgs.coreutils}/bin/mkdir -p ${writableDir}
-      ${pkgs.coreutils}/bin/chown -R jankclient:users ${writableDir}
+      ${pkgs.coreutils}/bin/chown -R jankclient:jankclient ${writableDir}
       ${pkgs.coreutils}/bin/cp -r ${inputs.jankclient}/* ${writableDir}/gitfiles
       ${lib.getExe pkgs.bun} install --cwd ${writableDir}
       ${lib.getExe pkgs.bun} gulp --cwd ${writableDir} --swc
