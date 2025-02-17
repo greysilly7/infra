0c4d492417a36f638c29a78c6ff4d6e4cdac9059
greysilly7 <greysilly7@gmail.com>
2025-02-17 01:05:30 -0500

    feat: enable Docker socket support and update service configuration for Podman compatibility
    Signed-off-by: greysilly7 <greysilly7@gmail.com>

--- system/services/docker.nix
+++ system/services/docker.nix
@@ -6,6 +6,7 @@
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
+      dockerSocket.enable = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
@@ -16,7 +17,7 @@
   environment.systemPackages = [
     pkgs.dive # look into docker image layers
     pkgs.podman-tui # status of containers in the terminal
-    pkgs.docker-compose # start group of containers for dev
-    #podman-compose # start group of containers for dev
+    #pkgs.docker-compose # start group of containers for dev
+    pkgs.podman-compose # start group of containers for dev
   ];
 }
