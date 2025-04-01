3342a832e7b1b1bfdd9fb7412f595b2bfab42bcd
greysilly7 <greysilly7@gmail.com>
2025-04-01 18:44:34 -0400

    Fix ExecStart command in Pterodactyl Wings service configuration to ensure proper execution of shell commands
    Signed-off-by: greysilly7 <greysilly7@gmail.com>

--- system/services/pterodactyl-wings.nix
+++ system/services/pterodactyl-wings.nix
@@ -19,7 +19,7 @@ in {
       WorkingDirectory = "/run/wings";
       LimitNOFILE = 4096;
       PIDFile = "/var/run/wings/daemon.pid";
-      ExecStart = "/bin/sh -c '/usr/bin/env mkdir /run/wings; /usr/bin/env cat /etc/pterodactyl/config.yml > /run/wings/config.yml; ${wingsBinary}/bin/wings --config /run/wings/config.yml'";
+      ExecStart = "/usr/bin/env sh -c '/usr/bin/env mkdir /run/wings; /usr/bin/env cat /etc/pterodactyl/config.yml > /run/wings/config.yml; ${wingsBinary}/bin/wings --config /run/wings/config.yml'";
       Restart = "on-failure";
       RestartSec = "5s";
     };
