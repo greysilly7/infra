5600807c9dab262225c4ad776987e8296ee89a95
greysilly7 <greysilly7@gmail.com>
2025-04-01 21:39:37 -0400

    Refactor pterodactyl-wings.nix to correctly read configuration from secrets path and ensure directory creation
    Signed-off-by: greysilly7 <greysilly7@gmail.com>

--- system/services/pterodactyl-wings.nix
+++ system/services/pterodactyl-wings.nix
@@ -1,7 +1,7 @@
 {lib, pkgs, config, ...}: let
   wingsConfig = ''
     # /etc/pterodactyl/configuration.yml managed by /etc/NixOS/wings.nix
-  '' + builtins.readFile config.sops.secrets.wings.path;
+  ''
   wingsBinary = pkgs.callPackage ../../pkgs/wings {};
 in {
   environment.etc."pterodactyl/config.yml".text = wingsConfig;
@@ -19,10 +19,11 @@ in {
       WorkingDirectory = "/run/wings";
       LimitNOFILE = 4096;
       PIDFile = "/run/wings/daemon.pid";
-      ExecStart = "${lib.getExe pkgs.bash} -c '/usr/bin/env mkdir /run/wings; /usr/bin/env cat /etc/pterodactyl/config.yml > /run/wings/config.yml; ${wingsBinary}/bin/wings --config /run/wings/config.yml'";
+      ExecStart = "${lib.getExe pkgs.bash} -c '/usr/bin/env mkdir -p /run/wings; cat ${config.sops.secrets.wings.path} > /run/wings/config.yml; ${wingsBinary}/bin/wings --config /run/wings/config.yml'";
       Restart = "on-failure";
       RestartSec = "5s";
     };
     wantedBy = ["multi-user.target"];
+    path = [ pkgs.uutils-coreutils-noprefix]
   };
 }
