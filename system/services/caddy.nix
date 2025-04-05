6cd2c6b31c5b1d39c69cff70d931cefbf80881d0
greysilly7 <greysilly7@gmail.com>
2025-04-05 00:19:22 -0400

    Enhance CORS headers in Caddy configuration for improved security and flexibility
    Signed-off-by: greysilly7 <greysilly7@gmail.com>

--- system/services/caddy.nix
+++ system/services/caddy.nix
@@ -69,9 +69,10 @@
         method OPTIONS
       }
       handle @options {
-        header Access-Control-Allow-Origin "*"
-        header Access-Control-Allow-Methods "*"
-        header Access-Control-Allow-Headers "*"
+        header Access-Control-Allow-Origin *
+        header Access-Control-Allow-Methods *
+        header Access-Control-Allow-Headers "Authorization, Content-Type, *"
+        header Access-Control-Allow-Credentials true
         header Access-Control-Max-Age "1728000"
         header Content-Type "text/plain; charset=utf-8"
         header Content-Length "0"
