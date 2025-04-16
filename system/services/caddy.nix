5d0e59b6c4b1e00b8eba6f06f188ceb1c6c6811e
greysilly7 <greysilly7@gmail.com>
2025-04-16 11:07:21 -0400

    chore: update CORS headers to allow any header
    Signed-off-by: greysilly7 <greysilly7@gmail.com>

--- system/services/caddy.nix
+++ system/services/caddy.nix
@@ -50,7 +50,7 @@
       handle @options {
         header Access-Control-Allow-Origin *
         header Access-Control-Allow-Methods "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-        header Access-Control-Allow-Headers "Authorization, Content-Type"
+        header Access-Control-Allow-Headers "Authorization, Content-Type, *" // Allow any header
         header Access-Control-Allow-Credentials true
         header Access-Control-Max-Age "1728000"
         header Content-Type "text/plain; charset=utf-8"
@@ -126,7 +126,7 @@
       handle @options {
         header Access-Control-Allow-Origin *
         header Access-Control-Allow-Methods *
-        header Access-Control-Allow-Headers "Authorization, Content-Type, *"
+        header Access-Control-Allow-Headers "Authorization, Content-Type, *" // Allow any header
         header Access-Control-Allow-Credentials true
         header Access-Control-Max-Age "1728000"
         header Content-Type "text/plain; charset=utf-8"
