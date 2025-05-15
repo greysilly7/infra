af9173c53e535633bcee563217c9064a1c7891aa
greysilly7 <greysilly7@gmail.com>
2025-05-15 14:21:51 -0400

    fix: disable systemd services to improve boot time

--- system/net/default.nix
+++ system/net/default.nix
@@ -75,6 +75,8 @@
   # slows down boot time
   systemd.services.NetworkManager-wait-online.enable = false;
   systemd.network.wait-online.enable = false;
+  systemd.services.systemd-networkd.stopIfChanged = false;
+  systemd.services.systemd-resolved.stopIfChanged = false;
 
   services.openssh = {
     enable = true;
